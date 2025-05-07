d34cf1c2c4d9544dd9ca110e0bda078b27d624fe
Y3fri <yeferguzman11@gmail.com>
2025-05-07 00:28:13 -0500

    mas productos y un boton

--- catalogos.js
+++ catalogos.js
@@ -214,3 +214,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// Botón para subir al inicio
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollToTopBtn.classList.add('visible');
+    } else {
+        scrollToTopBtn.classList.remove('visible');
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
